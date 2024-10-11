dffb92c14e6ada5ff464380bc0bc67b85e90b831
Jonathan Herring <124759864+jonathan-herring@users.noreply.github.com>
2024-10-10 23:43:16 -0700

    Refactor HomeScreen component structure

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,46 +1,30 @@
-import { StyleSheet, Image, Platform } from "react-native";
-import { HelloWave } from "@/components/HelloWave";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { StyleSheet, View } from "react-native";
 import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-      headerImage={
-        <Image
-          source={require("@/assets/images/partial-react-logo.png")}
-          style={styles.reactLogo}
-        />
-      }
-    >
-      <ThemedView style={styles.titleContainer}>
+    <View style={styles.container}>
+      <View style={styles.topContainer}>
         <ThemedText type="title">Hello, World!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
+      </View>
+      <View style={styles.middleContainer}>
         <ThemedText type="subtitle">Jonathan Herring</ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: "row",
+  container: {
+    flex: 1,
+  },
+  topContainer: {
+    marginTop: 50,
     alignItems: "center",
-    gap: 8,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: "absolute",
+  middleContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
